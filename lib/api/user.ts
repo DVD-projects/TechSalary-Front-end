5056bef4ab9bfc4726dab3c49a1d89638dc49321
Dexora Solutions <dinethdisanayaka11@gmail.com>
2026-02-18 10:00:48 +0530

    update: identity service part with bff-service

--- lib/api/user.ts
+++ lib/api/user.ts
@@ -1,6 +1,6 @@
 import { User, UpdateProfileRequest, PaginatedUsersResponse, UpdateUserPayload } from "./types"
 
-const API_BASE_URL = "http://127.0.0.1:8001/api/v1"
+const API_BASE_URL = "http://127.0.0.1:8000/api/v1"
 
 export class UserService {
 
